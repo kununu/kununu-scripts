3e29cd78d8f8cb0e59e913a7214cf6ef2a95144b
Tiago Pinheiro <tiago.pinheiro@kununu.com>
2019-05-08 14:56:09 +0100

    Added the pre-install for project commit

---
+++ code-standards.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+echo 'ola'
+pwd
+
+REPO_PATH=$(git rev-parse --show-toplevel)
+
+if [ ! -d $REPO_PATH ]
+then
+     chmod a+x ./gitHooks/PHP/pre-commit
+     mv ./gitHooks/PHP/pre-commit "$REPO_PATH/.git/hooks/pre-commit"
+     mv ./PHP/codeStandards/.php_cs "$REPO_PATH/.git/hooks/.php_cs"
+else
+   echo 'Git not found ...'
+fi
