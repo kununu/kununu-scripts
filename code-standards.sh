020d482698ddba87bf7bf4d48caa7e67cdbef040
Tiago Pinheiro <tiago.pinheiro@kununu.com>
2019-05-12 21:06:02 +0100

    Added debug to check why post-install-cmd fails

--- code-standards.sh
+++ code-standards.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
-echo 'ola'
-pwd
+echo 'ola' >> /tmp/test
+pwd >> /tmp/test
 
 REPO_PATH=$(git rev-parse --show-toplevel)
+echo $REPO_PATH >> /tmp/test
 
 if [ -d $REPO_PATH ]
 then
-     chmod a+x ./gitHooks/PHP/pre-commit
-     mv ./gitHooks/PHP/pre-commit "$REPO_PATH/.git/hooks/pre-commit"
-     mv ./PHP/codeStandards/.php_cs "$REPO_PATH/.git/hooks/.php_cs"
+     chmod a+x ./gitHooks/PHP/pre-commit >> /tmp/teste 2>&1
+     cp ./gitHooks/PHP/pre-commit "$REPO_PATH/.git/hooks/pre-commit"
+     cp ./PHP/codeStandards/.php_cs "$REPO_PATH/.git/hooks/.php_cs"
 else
    echo 'Git not found ...'
 fi
