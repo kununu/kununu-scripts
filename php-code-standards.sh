befa3f0e02863509727ee55c75bf58188c9f48d7
Tiago Pinheiro <tiago.pinheiro@kununu.com>
2019-05-21 15:10:31 +0100

    Fix variable name

--- php-code-standards.sh
+++ php-code-standards.sh
@@ -4,8 +4,8 @@ REPO_PATH=$(git rev-parse --show-toplevel)
 
 if [ -d $REPO_PATH ]
 then
-    PWD=`pwd`
-    chmod a+x "$PWD/gitHooks/PHP/pre-commit"
-    cp "$PWD/gitHooks/PHP/pre-commit" "$REPO_PATH/.git/hooks/pre-commit"
-    cp "$PWD/PHP/codeStandards/.php_cs" "$REPO_PATH/.git/hooks/.php_cs"
+    BASEDIR=$(dirname "$0")
+    chmod a+x "$BASEDIR/gitHooks/PHP/pre-commit"
+    cp "$BASEDIR/gitHooks/PHP/pre-commit" "$REPO_PATH/.git/hooks/pre-commit"
+    cp "$BASEDIR/PHP/codeStandards/.php_cs" "$REPO_PATH/.git/hooks/.php_cs"
 fi
