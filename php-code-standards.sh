320bb49f926f2f647085897c9ec7e3e00ff2a4f7
Tiago Pinheiro <tiago.pinheiro@kununu.com>
2019-05-21 14:46:03 +0100

    Correct path to php scripts

--- php-code-standards.sh
+++ php-code-standards.sh
@@ -1,15 +1,11 @@
 #!/bin/sh
 
-echo 'Starting PHP code standards ...'
-
 REPO_PATH=$(git rev-parse --show-toplevel)
-echo $REPO_PATH >> /tmp/test
 
 if [ -d $REPO_PATH ]
 then
-     chmod a+x ./gitHooks/PHP/pre-commit >> /tmp/teste 2>&1
-     cp ./gitHooks/PHP/pre-commit "$REPO_PATH/.git/hooks/pre-commit"
-     cp ./PHP/codeStandards/.php_cs "$REPO_PATH/.git/hooks/.php_cs"
-else
-   echo 'Git not found ...'
+    PATH=$(pwd)
+    chmod a+x "$PATH/gitHooks/PHP/pre-commit"
+    cp "$PATH/gitHooks/PHP/pre-commit" "$REPO_PATH/.git/hooks/pre-commit"
+    cp "$PATH/PHP/codeStandards/.php_cs" "$REPO_PATH/.git/hooks/.php_cs"
 fi
