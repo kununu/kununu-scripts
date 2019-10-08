967eb10b25f9f1ebbf270f6a81f86018f3d8a962
tiagopinheiro-kununu <47568091+tiagopinheiro-kununu@users.noreply.github.com>
2019-10-08 15:05:52 +0100

    Code to auto-update package kununu scripts (#1)
    * Code to auto-update package kununu scripts
    
    * Fix
    
    * Some more work
    
    * Added Code fixer command
    
    * Added Code fixer command
    
    * Fixes on pre-commit
    
    * Enforce delete file
    
    * Added readme and some code improvements
    
    * Added readme and some code improvements
    
    * Future work

--- src/PHPCodeStandards/Command/PHPCsFixerGitHookCommand.php
+++ src/PHPCodeStandards/Command/PHPCsFixerGitHookCommand.php
@@ -21,19 +21,57 @@ class PHPCsFixerGitHookCommand extends BaseCommand
         $output->writeln('<info>' . $this->getName() . '</info> Appling PHP CS Fixer Git Pre-Commit Hook ....');
         $path = dirname($this->getComposer()->getConfig()->getConfigSource()->getName()) . '/..';
 
-        $gitHooksPath = $path . '/.git/hooks';
-        if (!is_dir($gitHooksPath)) {
-            $this->getIO()->writeError('<error>GIT folder not found at ' . $gitHooksPath . '</error>');
+        $gitPath = $path . '/.git';
+        if (!is_dir($gitPath)) {
+            $this->getIO()->writeError('<error>GIT folder not found at "' . $gitPath . '"</error>');
 
             return;
         }
 
         $currentFolder = __DIR__;
+        $this->addGitHook($gitPath, $currentFolder . '/../git-pre-commit', 'pre-commit');
 
-        copy($currentFolder . '/../php_cs', $gitHooksPath . '/.php_cs');
-        copy($currentFolder . '/../git-pre-commit', $gitHooksPath . '/pre-commit');
-        chmod($gitHooksPath . '/pre-commit', 0777);
+        // Add php-cs-fixer rules to be available on .git folder.
+        $this->addLinkToGitFolder(
+            $gitPath,
+            '../../services/vendor/kununu/scripts/src/PHPCodeStandards/php_cs',
+            '.php_cs'
+        );
+
+        // Add php-cs-fixer bin to be available on .git folder.
+        $this->addLinkToGitFolder(
+            $gitPath,
+            '../../services/vendor/kununu/scripts/vendor/bin/php-cs-fixer',
+            'php-cs-fixer'
+        );
 
         $output->writeln('<info>' . $this->getName() . '</info> .... Git Hook Applied');
+    }
+
+    private function addGitHook(string $gitPath, string $file, string $hookName): void
+    {
+        $hookPath = $gitPath . '/hooks/' . $hookName;
+        if (is_file($hookPath)) {
+            unlink($hookPath);
+        }
+
+        copy($file, $hookPath);
+        chmod($hookPath, 0777);
+    }
+
+    private function addLinkToGitFolder(string $gitPath, string $origin, string $linkName): void
+    {
+        // In order to make it clear a folder is created named kununu with the dependencies for pre-commit.
+        $kununuBinPath = $gitPath . '/kununu';
+        if (!is_dir($kununuBinPath)) {
+            mkdir($kununuBinPath);
+        }
+
+        $kununuLink = $kununuBinPath . '/' . $linkName;
+        if (is_link($kununuLink)) {
+            unlink($kununuLink);
+        }
+
+        symlink($origin, $kununuLink);
     }
 }
